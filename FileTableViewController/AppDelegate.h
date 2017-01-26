be281a5cd70027a63b736f1086c182c6efbe154d
Zvigun Alexandr <mircamnia@ukr.net>
2017-01-26 19:05:46 +0200

    Initial Commit

---
+++ FileTableViewController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FileTableViewController
+//
+//  Created by Mac on 26.01.17.
+//  Copyright © 2017 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
